d7b125a6c6c6a7e31cf614de79aa3e8bde477737
Rogelio Trejo Estrada <trejo.estrada.rogelio@gmail.com>
2022-05-17 22:01:46 -0500

    Privilages to the user

--- server/database/user.sql
+++ server/database/user.sql
@@ -1,1 +1,5 @@
-CREATE USER 'task-user'@'localhost' IDENTIFIED BY '123456';
+# Create the user
+CREATE USER 'task-user' @'localhost' IDENTIFIED BY '123456';
+
+# Add all PRIVILEGES in the database
+GRANT ALL PRIVILEGES ON `todo-app`.* TO 'task-user' @'localhost';
